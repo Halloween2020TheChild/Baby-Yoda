d28c1f05f5b487a415276fd793ead16d9c255d3b
Kevin Harrington <harrington@wpi.edu>
2021-03-19 12:08:16 -0400

    whitespace

--- Core.groovy
+++ Core.groovy
@@ -4,7 +4,6 @@ import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
 import com.neuronrobotics.bowlerstudio.vitamins.Vitamins
 
 import eu.mihosoft.vrl.v3d.*
-
 File Left_Hand = ScriptingEngine.fileFromGit("https://github.com/vermontolympian/Baby-Yoda.git",
 "DownRes-LeftInsert-4mm.stl");					
 CSG Left = Vitamins.get(Left_Hand)							//Get hand STL	
