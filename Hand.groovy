7ac3e3a7eddafc0cf0edbe26565fde0a862d5500
Kevin Harrington <mad.hephaestus@gmail.com>
2021-03-22 11:34:53 -0400

    updated

--- Hand.groovy
+++ Hand.groovy
@@ -5,30 +5,39 @@ import eu.mihosoft.vrl.v3d.CSG
 import eu.mihosoft.vrl.v3d.*
 
 File Right_Hand = ScriptingEngine.fileFromGit("https://github.com/vermontolympian/Baby-Yoda.git",
-"DownRes-Right-Hand.stl");								
+"DownRes-Right-Hand.stl");		
+
+File core_file = ScriptingEngine.fileFromGit("https://github.com/vermontolympian/Baby-Yoda.git",
+	"DownRes-RightInsert-4mm.stl");
+CSG core = Vitamins.get(core_file)
+				.toZMin()
+core=core
+				.movex(-core.centerX-0.75)
+				.movey(-core.centerY)
+	
 CSG Right  = Vitamins.get(Right_Hand)						//Get hand STL
 			.scale(2)									//Scale to "life-like" size
-			.toXMin()
-			.toYMin()
 			.toZMin()
-CSG Cube = new Cube(70,30,100).toCSG()						//Create front mold piece
-			.movex(20)
-			.movey(5)
-			.movez(40)
+Right=Right
+			.movex(-Right.centerX)
+			.movey(-Right.centerY)
 			
-CSG Cube2 = new Cube(70,30,100).toCSG()						//Create back mold piece
-			.movex(20)
-			.movez(40)
+def yDepthOfPartLine =6
+CSG Cube = new Cube(70,30,100).toCSG()						//Create front mold piece
+			.toZMin()
+			.movez(-15)
 			.toYMin()
-			.movey(Cube.getMaxY())
+			.movey(yDepthOfPartLine)
+CSG cube2=		Cube
+				.toYMax()
+				.movey(yDepthOfPartLine)
 
 CSG Cylinder = (CSG)(ScriptingEngine.gitScriptRun(
-            "https://github.com/vermontolympian/Baby-Yoda.git", // git location of the library
+            "https://github.com/Halloween2020TheChild/Baby-Yoda.git", // git location of the library
             "handPlug.groovy" ,null))
 			.toZMax()
-			.movey(20)
-			.movex(18)
-			.movez(0.1)
+			
+return [Right,core.union(Cylinder),Cube,cube2]
 
 CSG Bolt1 = new Cylinder (2.75,2.75,60,(int)8).toCSG()			//Create bolt hole 1
 			.rotx(90)
@@ -67,12 +76,12 @@ CSG Pry2 = new Cube (20,4,20).toCSG()						//Create pry location 2
 def stuff = [Cylinder,Right,Bolt1,Bolt2,Bolt3,Bolt4,Pry1,Pry2]	//create group of stuff to be cut out
 
 CSG Palm = Cube.difference(stuff)							//Cut 'stuff' out of palm side of mold
-			.rotx(-90)
-			.toZMin()
+//			.rotx(-90)
+//			.toZMin()
 				
 CSG Back = Cube2.difference(stuff)							//Cut 'stuff' out of back side of mold
-			.rotx(90)
-			.movey(40)
-			.toZMin()
+//			.rotx(90)
+//			.movey(40)
+//			.toZMin()
 
 return [Palm,Back];										//Output mold parts
--- handPlug.groovy
+++ handPlug.groovy
@@ -1,4 +1,14 @@
 import eu.mihosoft.vrl.v3d.CSG
 import eu.mihosoft.vrl.v3d.Cylinder
+import eu.mihosoft.vrl.v3d.RoundedCube
 
-return  new Cylinder (15,11,20,(int)30).toCSG()			//Create pouring slot geometry
+CSG rc= new RoundedCube(	40,// X dimention
+				40,// Y dimention
+				40//  Z dimention
+				)
+				.cornerRadius(4)// sets the radius of the corner
+				.toCSG()// converts it to a CSG tor display
+				.toZMax()
+				.movez(0.5)
+				.movex(-5)
+				return rc
