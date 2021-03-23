90eab1c7080b78dba7f8a6a7bd6e0b9123bb8821
Kevin Harrington <mad.hephaestus@gmail.com>
2021-03-22 22:27:26 -0400

    updated

--- LeftEar.groovy
+++ LeftEar.groovy
@@ -9,6 +9,9 @@ import eu.mihosoft.vrl.v3d.svg.SVGLoad
 import eu.mihosoft.vrl.v3d.*
 def url="https://github.com/Halloween2020TheChild/Baby-Yoda.git"
 def branch="master"
+double moldY = 90
+double moldX = 90
+double moldZ = 200
 
 File earFile = ScriptingEngine.fileFromGit(url,
 		"Left Ear-DownRes.stl");
@@ -31,5 +34,19 @@ if(!earCoreFile.exists()) {
 	ScriptingEngine.pushCodeToGit(url, branch, "LeftEar-DownRes-4mmInset.stl", earCore.toStlString(), "Making ear core ", true)
 }else
 	earCore=Vitamins.get(earCoreFile)
+File f = ScriptingEngine
+.fileFromGit(
+	"https://github.com/Halloween2020TheChild/Baby-Yoda.git",//git repo URL
+	"master",//branch
+	"draftlineEar.SVG"// File from within the Git repo
+	)
+println "Extruding SVG "+f.getAbsolutePath()
+SVGLoad s = new SVGLoad(f.toURI())
+CSG draftLine = s.extrudeLayerToCSG(moldX*2,"X")
+				.rotx(-90)
+				.rotz(-90)
+				.movex(-moldX)
+				.rotz(-7)
 
-return [ear,earCore]
+
+return [ear,earCore,draftLine]
