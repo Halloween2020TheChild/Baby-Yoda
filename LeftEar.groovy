274b37099b88af0090c692e06d2cdeddfb88943a
Kevin Harrington <mad.hephaestus@gmail.com>
2021-03-22 21:01:49 -0400

    Making ear core

--- LeftEar.groovy
+++ LeftEar.groovy
@@ -1,1 +1,33 @@
-//Your code here
+//Your code hereimport com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
+import java.nio.file.Paths
+
+import com.neuronrobotics.bowlerstudio.scripting.ScriptingEngine
+import com.neuronrobotics.bowlerstudio.vitamins.Vitamins
+
+import eu.mihosoft.vrl.v3d.CSG
+import eu.mihosoft.vrl.v3d.svg.SVGLoad
+import eu.mihosoft.vrl.v3d.*
+def url="https://github.com/Halloween2020TheChild/Baby-Yoda.git"
+def branch="master"
+
+File earFile = ScriptingEngine.fileFromGit(url,
+		"Left Ear-DownRes.stl");
+//Vitamins.clear();
+CSG ear  = Vitamins.get(earFile)
+//		.roty(90)
+//		.toZMin()
+//ear=ear.movex(-ear.centerX)
+//			.movey(-ear.centerY)		
+File earCoreFile = ScriptingEngine.fileFromGit(url,
+		"LeftEar-DownRes-4mmInset.stl");
+CSG earCore; 
+if(!earCoreFile.exists()) {
+	println "Making ear core"
+	earCore=ear.toolOffset(-4)
+	ScriptingEngine.pushCodeToGit(url, branch, "LeftEar-DownRes-4mmInset.stl", ear.toStlString(), "Making ear core ", true)
+}
+
+//FileUtil.write(Paths.get(earFile.getAbsolutePath()),
+//		ear.toStlString());
+
+return [ear,earCore]
