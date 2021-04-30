b5a5984acde9587f2b427cb598bfca6636483ab7
Kevin Harrington <mad.hephaestus@gmail.com>
2021-04-30 14:14:48 -0400

    nremove unused files

--- LeftEar.groovy
+++ LeftEar.groovy
@@ -98,15 +98,6 @@ CSG ear  = Vitamins.get(earFile)
 //		.toZMin()
 //ear=ear.movex(-ear.centerX)
 //			.movey(-ear.centerY)		
-File earCoreFile = ScriptingEngine.fileFromGit(url,
-		"LeftEar-DownRes-4mmInset.stl");
- 
-if(!earCoreFile.exists()) {
-	println "Making ear core"
-	earCore=ear.toolOffset(-4)
-	ScriptingEngine.pushCodeToGit(url, branch, "LeftEar-DownRes-4mmInset.stl", earCore.toStlString(), "Making ear core ", true)
-}else
-	earCore=Vitamins.get(earCoreFile)
 
 
 CSG.setProgressMoniter(new ICSGProgress() {
