2bbe5f5cdc711082c0a23b5cf90449de2c64a5b3
Kevin Harrington <harrington@wpi.edu>
2021-05-05 10:01:17 -0400

    added fingertips vents

--- Hand.groovy
+++ Hand.groovy
@@ -25,6 +25,7 @@ CSG Right  = Vitamins.get(Right_Hand)						//Get hand STL
 Right=Right
 			.movex(-Right.centerX)
 			.movey(-Right.centerY)
+			.movez(-1)
 			
 CSG moldCore = new Cube(70,60,moldHeight+coneheight).toCSG()						//Create front mold piece
 			.toZMin()
@@ -77,7 +78,7 @@ CSG Pry = new Cube (15,4,20).toCSG()						//Create pry location 2
 
 CSG pry1 = Pry.toXMax().movex(moldCore.getMaxX()+1)
 					.movey(1.5)// hand adjust to meet the part line
-CSG pry2 = Pry.toXMin().movex(moldCore.getMinX()-1)
+CSG pry2 = Pry.toXMin().movex(moldCore.getMinX()-8)
 					.movey(4.5)// hand adjust to meet the part line
 def pourLocation = new Transform()
 		.movez(moldHeight+moldLowering)
@@ -88,7 +89,7 @@ CSG pourHole = new Cylinder(2, // Radius at the bottom
                       		40, // Height
                       		(int)8 //resolution
                       		).toCSG()//convert to CSG to display    
-				.movez(-8)
+				.movez(-12)
 				.transformed(pourLocation)
 				
 CSG pourCone =new Cylinder(2, // Radius at the bottom
@@ -98,9 +99,70 @@ CSG pourCone =new Cylinder(2, // Radius at the bottom
                       		).toCSG()//convert to CSG to display  
 							.transformed(pourLocation)
 							.intersect(moldCore.movez(40))
-				
-def moldA = moldCore.difference(draftLine,Cylinder,Right ,lower,upperL,upperR,pry1,pry2,pourHole,pourCone).rotx(90).movey(45)
-def moldB = draftLine.difference(Cylinder,Right ,lower,upperL,upperR,pry1,pry2,pourHole,pourCone).rotx(-90).movey(-45)
+def toVent(def list) {
+	def paths=[]
+	for(int i=0;i<list.size()-1;i++) {
+		paths.add(list[i].union(list[i+1]).hull())
+	}
+	return CSG.unionAll(paths)
+}
+def vents = [new Cube(4).toCSG()
+			.toZMin()
+			.movex(9)
+			.movey(4),
+ new Cube(4).toCSG()
+			.toZMin()
+			.movex(30)
+			.movez(30),
+new Cube(4).toCSG()
+			.toZMin()
+			.movex(30)
+			.movez(100),
+			new Cube(4).toCSG()
+			.toZMin()
+			.movex(100)
+			.movez(100)
+			]
+def vents2 = [new Cube(4).toCSG()
+				.toZMin()
+				.movex(-18)
+				.movey(0),
+	 new Cube(4).toCSG()
+				.toZMin()
+				.movey(4)
+				.movex(-30)
+				.movez(20),
+	new Cube(4).toCSG()
+				.toZMin()
+				.movey(4)
+				.movex(-30)
+				.movez(100),
+				new Cube(4).toCSG()
+				.toZMin()
+				.movey(20)
+				.movex(-100)
+				.movez(100)
+				]
+CSG vent = toVent(vents)
+			.union(new Cube(12,4,4).toCSG()
+				.toZMin()
+				.toXMin()
+				.movez(52)
+				.movey(0)
+				.movex(18))
+
+CSG vent2 = toVent(vents2)
+.union(new Cube(16,4,4).toCSG()
+	.toZMin()
+	.toXMax()
+	.movez(69)
+	.movey(2)
+	.movex(-15))
+
+
+									
+def moldA = moldCore.difference(draftLine,Cylinder,Right ,lower,upperL,upperR,pry1,pry2,pourHole,pourCone,vent,vent2)//.rotx(90).movey(45)
+def moldB = draftLine.difference(Cylinder,Right ,lower,upperL,upperR,pry1,pry2,pourHole,pourCone,vent,vent2).rotx(-90).movey(-45)
 def moldCoreFinal = core.union(rc.difference(lower))
 def moldALeft = moldA.mirrory().movex(100)
 def moldBLeft  = moldB.mirrory().movex(100)
@@ -112,7 +174,6 @@ moldCoreFinal.setName("RightMoldCore")
 moldALeft.setName("LeftMoldA")
 moldBLeft.setName("LeftMoldB")
 moldCoreFinalLeft.setName("LeftMoldCore")
-
 
 return [moldB,moldCoreFinal,moldA,moldALeft,moldBLeft,moldCoreFinalLeft]
 
